fc3f293f6ae45645c432fe30ec15f286a04adca9
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-06-14 16:52:27 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let name = "Kaitlin";
+	let age = 34;
+
+	function incrementAge(){
+		age += 1;
+	}
+
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, I am {age} years old!</h1>
+<button on:click="{incrementAge}">Add to age</button>
